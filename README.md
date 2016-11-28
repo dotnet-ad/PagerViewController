f5cd156746f89bd6af1938779de6f9277abcb291
Aloïs Deniel <alois.deniel@gmail.com>
2016-11-29 00:55:01 +0100

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # PagerViewController
-Tabs at the top of the screen for Xamarin.iOS.
+
+A basic pager ViewController for Xamarin.iOS.
+
+![Documentation/PagerAnimation.gif](Documentation/PagerAnimation.gi)
+
+## Getting started
+
+```csharp
+var colors = new[]
+{
+	UIColor.FromRGB(76,217,100),
+	UIColor.FromRGB(52,170,220),
+	UIColor.FromRGB(88,86,214),
+ 	UIColor.FromRGB(255,45,85),
+};
+
+var pages = new[]
+{
+	new UIViewController() { Title = "First" },
+	new UIViewController() { Title = "Second" },
+	new UIViewController() { Title = "Third" },
+	new UIViewController() { Title = "Last" },
+};
+
+var style = new PagerStyle()
+{
+    SelectedStripColors = colors,
+};
+
+var pager = new PagerViewController(style, pages);
+
+var nav = new UINavigationController(pager);
+nav.NavigationBar.Translucent = false;
+```
+
+## Roadmap / Ideas
+
+- [X] ViewController
+- [ ] Setup NuGet
+- [ ] More customization
+- [ ] Dynamic tabs modification
+- [ ] Storyboard integration
+
+
+## About
+
+This view controller did the trick but is far from a real polished software and needs some work for real customization. Meanwhile, it can save some time to someone, maybe ... And I'll not lost it!
+
+## Contributions
+
+Contributions are welcome! If you find a bug please report it and if you want a feature please report it.
+
+If you want to contribute code please file an issue and create a branch off of the current dev branch and file a pull request.
+
+### License
+
+MIT © [Aloïs Deniel](http://aloisdeniel.github.io)
---
+++ Sources/PagerViewController/PagerViewController.cs
@@ -0,0 +1,98 @@
+﻿namespace Pager
+{
+	using System.Linq;
+	using CoreGraphics;
+	using UIKit;
+
+	public class PagerViewController : UIViewController
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Pager.PagerViewController"/> class.
+		/// </summary>
+		/// <param name="style">The style of the pager.</param>
+		/// <param name="children">All children view controllers.</param>
+		public PagerViewController(PagerStyle style, params UIViewController[] children)
+		{
+			this.children = children;
+			this.style = new PagerStyle(style);
+		}
+
+		#endregion
+
+		#region Fields
+
+		private PagerStyle style;
+
+		private UIPageViewController pager;
+
+		private UIViewController[] children;
+
+		private PagerBar bar;
+
+		#endregion
+
+		#region Lifecycle
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			this.View.BackgroundColor = this.style.PagesBackgroundColor;
+
+			this.bar = new PagerBar(this.style)
+			{
+				Frame = new CGRect(0, 0, this.View.Bounds.Width, this.style.BarHeight),
+				AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleBottomMargin,
+			};
+
+			this.bar.Sections = this.children.Select(vc => vc.Title).ToArray();
+			this.bar.TabSelected += OnTabSelected; ;
+
+			this.View.AddSubview(bar);
+
+			// Pager
+
+			this.pager = new UIPageViewController(UIPageViewControllerTransitionStyle.Scroll, UIPageViewControllerNavigationOrientation.Horizontal);
+			this.pager.DidFinishAnimating += OnPageSwiped;
+			this.pager.DataSource = new PagerDataSource(children);
+			this.pager.SetViewControllers(children.Take(1).ToArray(), UIPageViewControllerNavigationDirection.Forward, false, s => { });
+
+			this.AddChildViewController(pager);
+			pager.DidMoveToParentViewController(this);
+
+			this.View.AddSubview(this.pager.View);
+		}
+
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+
+			var pagerView = this.pager.View;
+			pagerView.Frame = new CGRect(0, this.style.BarHeight, this.View.Bounds.Width, this.View.Bounds.Height - this.style.BarHeight);
+			pagerView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleBottomMargin;
+		}
+
+		#endregion
+
+		#region Tab selection
+
+		private void OnPageSwiped(object sender, UIPageViewFinishedAnimationEventArgs e)
+		{
+			if (e.Completed)
+			{
+				var index = this.children.ToList().IndexOf(this.pager.ViewControllers[0]);
+				this.bar.Select(index, true);
+			}
+		}
+
+		private void OnTabSelected(object sender, PagerBar.ValueChangedEventArgs e)
+		{
+			var direction = (e.OldValue < e.NewValue) ? UIPageViewControllerNavigationDirection.Forward : UIPageViewControllerNavigationDirection.Reverse;
+			this.pager.SetViewControllers(new[] { children[e.NewValue] }, direction, true, s => { });
+		}
+
+		#endregion
+	}
+}
